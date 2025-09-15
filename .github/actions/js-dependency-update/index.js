37929290e10aee882ac3ad7c1ae9f92187ddd407
Wilian Huanca <whuanca@okode.com>
2025-09-15 22:11:10 +0200

    feat(17-custom-actions): Add first version of JS custom action

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,44 @@
 const core = require("@actions/core");
+const { exec } = require("@actions/exec");
 
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9-_\.\/]+$/.test(branchName);
+const valdiateDirectoryName = ({ directoryName }) => /^[a-zA-Z0-9-_\-\/]+$/.test(directoryName);
 async function run() {
-  core.info("I am a custom JS action");
+  /**
+   * Parse inputs: base-branch from which to check for updates
+   * target branch to use to create the PR
+   */
+  const baseBranch = core.getInput("base-branch");
+  const targetBranch = core.getInput("target-branch");
+  const workingDirectory = core.getInput("working-directory");
+  const ghToken = core.getInput("gh-token");
+  const debug = core.getInput("debug");
+
+  if(!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(`Invalid base branch name: ${baseBranch}`);
+    return;
+  }
+  if(!validateBranchName({ branchName: targetBranch })) {
+    core.setFailed(`Invalid base branch name: ${targetBranch}`);
+    return;
+  }
+  if(!valdiateDirectoryName({ directoryName: workingDirectory })) {
+    core.setFailed(`Invalid working directory name: ${workingDirectory}`);
+    return;
+  }
+  core.info(`[js-dependency-update]: Base branch: ${baseBranch}`);
+  core.info(`[js-dependency-update]: Target branch: ${targetBranch}`);
+  core.info(`[js-dependency-update]: Working directory: ${workingDirectory}`);
+
+  await exec(`npm update`, [], { cwd: workingDirectory });
+
+  const gitStatus = await exec.getExecOutput(`git status -s package*.json`, [], { cwd: workingDirectory });
+  if (gitStatus.stdout.length > 0) {
+    core.info(`[js-dependency-update]: There are updates available`);
+  } else {
+    core.info(`[js-dependency-update]: No updates to commit`);
+  }
+
 }
 
 run();
