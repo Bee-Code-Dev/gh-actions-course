7b9935a9881276a285b2aef82bd3316e020c7426
Wilian Huanca <whuanca@okode.com>
2025-10-04 21:09:24 +0200

    feat(18-custom-actions-3): Add workflows file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,16 +2,17 @@ import os
 import requests
 import time
 
-def ping_url(url, timeout=5, max_retries=3, retry_interval=2):
+def ping_url(url, max_retries, retry_interval):
     trials = 0
 
     while trials < max_retries:
       try:
-          response = requests.get(url, timeout=timeout)
-          print(f"URL {url} is reachable with status code {response.status_code}")
-          return True
+          response = requests.get(url)
+          if response.status_code == 200:
+            print(f"URL {url} is reachable with status code {response.status_code}")
+            return True
       except requests.ConnectionError as e:
-          print(f"Connection error: {e}. Retrying in {retry_interval} seconds...")
+          print(f"Connection error url{url}: Retrying in {retry_interval} seconds...")
           time.sleep(retry_interval)
           trials += 1
       except requests.exceptions.MissingSchema as e:
@@ -20,11 +21,10 @@ def ping_url(url, timeout=5, max_retries=3, retry_interval=2):
     return False
 
 def run():
-    web_url = os.getenv("INPUT_URL", "https://www.example.com")
-    timeout = int(os.getenv("INPUT_TIMEOUT", "5"))
-    max_retries = int(os.getenv("INPUT_MAX_RETRIES", "3"))
-    retry_interval = int(os.getenv("INPUT_RETRY_INTERVAL", "2"))
-    websi_reachable = ping_url(web_url, timeout, max_retries, retry_interval)
+    web_url = os.getenv("INPUT_URL")
+    max_retries = int(os.getenv("INPUT_MAX_RETRIES"))
+    retry_interval = int(os.getenv("INPUT_RETRY_INTERVAL"))
+    websi_reachable = ping_url(web_url, max_retries, retry_interval)
 
     if not websi_reachable:
         raise Exception(f"Website {web_url} is malformed or unreachable after {max_retries} attempts.")
