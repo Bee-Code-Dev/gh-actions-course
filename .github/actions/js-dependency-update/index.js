b11814d5b1ce15e844c4813fc6c924a281713ee0
Wilian Huanca <whuanca@okode.com>
2025-10-04 19:24:13 +0200

    feature(17-custom-actions-2): Update js file

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -4,70 +4,94 @@ const github = require("@actions/github");
 
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9-_\.\/]+$/.test(branchName);
 const valdiateDirectoryName = ({ directoryName }) => /^[a-zA-Z0-9-_\-\/]+$/.test(directoryName);
+const setupLogger = ({ debug, prefix } = {debug: false, prefix: ''}) => ({
+  debug: (msg) => {
+    if (debug) {
+      core.info(`DEBUG: ${prefix} ${prefix ? ':' : ''}${msg}`);
+    }
+  },
+  error: (msg) => {
+    core.error(`${prefix} ${prefix ? ':' : ''}${msg}`);
+  },
+  info: (msg) => {
+    core.info(`INFO: ${prefix} ${prefix ? ':' : ''}${msg}`);
+  }
+})
+
 async function run() {
   /**
    * Parse inputs: base-branch from which to check for updates
-   * target branch to use to create the PR
+   * head branch to use to create the PR
    */
   const baseBranch = core.getInput("base-branch", { required: true });
-  const targetBranch = core.getInput("target-branch", { required: true });
+  const headBranch = core.getInput("head-branch", { required: true });
   const workingDirectory = core.getInput("working-directory", { required: true });
   const ghToken = core.getInput("gh-token", { required: true });
-  const debug = core.getInput("debug");
+  const debug = core.getBooleanInput("debug");
+  const logger = setupLogger({ debug, prefix: '[js-dependency-update]' });
+
   const commonExcecOptions = {
     cwd: workingDirectory
   }
+  const gitHubSetup = async () => {
+    await exec(`git config --global user.name "gh-automation"`);
+    await exec(`git config --global user.email "gh-automation@email.com"`);
+  }
 
+  logger.debug('Validating inputs...');
+  logger.debug(`Inputs: baseBranch=${baseBranch}, headBranch=${headBranch}, workingDirectory=${workingDirectory}, debug=${debug}`);
   if(!validateBranchName({ branchName: baseBranch })) {
-    core.setFailed(`Invalid base branch name: ${baseBranch}`);
+    core.setFailed(`Invalid base-branch name: ${baseBranch}`);
     return;
   }
-  if(!validateBranchName({ branchName: targetBranch })) {
-    core.setFailed(`Invalid base branch name: ${targetBranch}`);
+  if(!validateBranchName({ branchName: headBranch })) {
+    core.setFailed(`Invalid head-branch name: ${headBranch}`);
     return;
   }
   if(!valdiateDirectoryName({ directoryName: workingDirectory })) {
     core.setFailed(`Invalid working directory name: ${workingDirectory}`);
     return;
   }
-  core.info(`[js-dependency-update]: Base branch: ${baseBranch}`);
-  core.info(`[js-dependency-update]: Target branch: ${targetBranch}`);
-  core.info(`[js-dependency-update]: Working directory: ${workingDirectory}`);
+  logger.debug(`Base branch: ${baseBranch}`);
+  logger.debug(`Head branch: ${headBranch}`);
+  logger.debug(`Working directory: ${workingDirectory}`);
 
+  logger.debug('Checking for package updates...');
   await exec(`npm update`, [], { ...commonExcecOptions });
 
   const gitStatus = await getExecOutput(`git status -s package*.json`, [], { ...commonExcecOptions });
   if (gitStatus.stdout.length > 0) {
-    core.info(`[js-dependency-update]: There are updates available`);
-    await exec(`git config --global user.name "gh-automation"`);
-    await exec(`git config --global user.email "gh-automation@email.com"`);
-    await exec(`git checkout -b ${targetBranch}`, [], {
+    logger.debug(`There are updates available`);
+    logger.debug(`Setting up git...`);
+    await gitHubSetup();
+    logger.debug(`Commiting and pushing changes in package.json and package-lock.json...`);
+    await exec(`git checkout -b ${headBranch}`, [], {
       ...commonExcecOptions
     });
     await exec(`git add package.json package-lock.json`, [], { ...commonExcecOptions });
     await exec(`git commit -m "chore: update NPM dependencies"`, [], { ...commonExcecOptions });
-    await exec(`git push --set-upstream origin ${targetBranch} --force`, [], { ...commonExcecOptions });
+    await exec(`git push --set-upstream origin ${headBranch} --force`, [], { ...commonExcecOptions });
+    logger.debug(`Fetching octokit API...`);
     const octokit = github.getOctokit(ghToken);
     try {
+      logger.debug(`Creating PR...`);
       await octokit.rest.pulls.create({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         title: `Update NPM dependencies`,
         body: `This PR updates the NPM dependencies to their latest versions`,
         base: baseBranch,
-        head: targetBranch,
+        head: headBranch,
       });
-      core.error(`[js-dependency-update]: Created PR successfully`);
+      logger.info(`Created PR successfully`);
     } catch (error) {
-      core.error(`[js-dependency-update]: Could not create PR, it might already exist`);
+      logger.error(`Something went wrong creating the PR: ${error.message}`);
       core.setFailed(error.message);
-      core.error(error);
+      logger.error(error);
     }
-    core.info(`[js-dependency-update]: Created PR successfully`);
   } else {
-    core.info(`[js-dependency-update]: No updates to commit`);
+    logger.info(`No updates at this point in time`);
   }
-
 }
 
 run();
