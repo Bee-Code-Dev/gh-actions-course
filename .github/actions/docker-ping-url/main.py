d15fc1c8536b30675dbf31247e206b1f29048011
Wilian Huanca <whuanca@okode.com>
2025-10-04 20:56:26 +0200

    feat(18-custom-actions-3): Add workflows file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -6,17 +6,17 @@ def ping_url(url, timeout=5, max_retries=3, retry_interval=2):
     trials = 0
 
     while trials < max_retries:
-        try:
-            response = requests.get(url, timeout=timeout)
-            print(f"URL {url} is reachable with status code {response.status_code}")
-            return True
-        except requests.ConnectionError as e:
-            print(f"Connection error: {e}. Retrying in {retry_interval} seconds...")
-            time.sleep(retry_interval)
-            trials += 1
-        except requests.exceptions.MissingSchema as e:
-            print(f"Invalid URL schema: {e}. Please check the URL format.")
-            return False
+      try:
+          response = requests.get(url, timeout=timeout)
+          print(f"URL {url} is reachable with status code {response.status_code}")
+          return True
+      except requests.ConnectionError as e:
+          print(f"Connection error: {e}. Retrying in {retry_interval} seconds...")
+          time.sleep(retry_interval)
+          trials += 1
+      except requests.exceptions.MissingSchema as e:
+          print(f"Invalid URL schema: {e}. Please check the URL format.")
+          return False
     return False
 
 def run():
